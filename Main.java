5903b7a418ad19da106e079bb57db395866a386b
raflyaulya <auliarafli819@gmail.com>
2023-12-25 21:12:14 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Monitor monitor=new Monitor();
+        Monitor monitor = new Monitor(); // Создание объекта Monitor
 
-        Thread producerThread=new Thread(monitor::producer);
-        Thread consumerThread=new Thread(monitor::consumer);
+        Thread producerThread = new Thread(monitor::producer); // Создание потока для производителя, ссылаясь на метод producer в объекте monitor
+        Thread consumerThread = new Thread(monitor::consumer); // Создание потока для потребителя, ссылаясь на метод consumer в объекте monitor
 
-        producerThread.start();
-        consumerThread.start();
+        producerThread.start(); // Запуск выполнения потока производителя
+        consumerThread.start(); // Запуск выполнения потока потребителя
 
     }
 }
